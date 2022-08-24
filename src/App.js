a9a668af18171eccb593e98e120ba335e3f44d10
Khalidprithy <khalidprithy@gmail.com>
2022-08-25 03:23:39 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import { Toaster } from 'react-hot-toast';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login/Login';
+import SignUp from './components/Login/SignUp';
+import Blog from './components/Pages/Blog';
+import Dashboard from './components/Pages/Dashboard';
+import Inventory from './components/Pages/Inventory';
+import Purchase from './components/Pages/Purchase';
+import Navbar from './components/Shared/Navbar';
+import NotFound from './components/Shared/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <p>Hi</p>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/inventory' element={
+          <Inventory></Inventory>
+
+        }></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/purchase' element={<Purchase></Purchase>}></Route>
+        <Route path='/login' element={<Login></Login>} />
+        <Route path='/signup' element={<SignUp></SignUp>} />
+
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
       <Toaster />
     </div>
   );
---
+++ src/components/Shared/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+
+    const menuItems = <>
+        <li className='font-semibold text-gray-600'><Link to='/'>Dashboard</Link></li>
+        <li className='font-semibold text-gray-600'><Link to='/inventory'>Inventory</Link></li>
+        <li className='font-semibold text-gray-600'><Link to='/blog'>Blog</Link></li>
+    </>
+
+
+    return (
+        <div>
+            <div className="navbar bg-neutral">
+                <div className="navbar-start flex-1">
+                    <div className="dropdown">
+                        <label tabIndex="0" className="btn btn-ghost md:hidden lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex="1" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-accent rounded-box w-48">
+                            {menuItems}
+                        </ul>
+                    </div>
+                    <Link to='/' className="btn btn-ghost normal-case text-xl text-secondary">Blooming Beauty</Link>
+                </div>
+                <div className="navbar-center hidden md:flex lg:flex-auto">
+                    <ul className="menu menu-horizontal p-0 md:pr-8">
+                        {menuItems}
+                    </ul>
+                </div>
+                <div className="flex-none">
+
+                    <div className="dropdown dropdown-end">
+                        <label tabindex="0" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="https://placeimg.com/80/80/people" alt='' />
+                            </div>
+                        </label>
+                        <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <p className="justify-between">Profile</p>
+                            </li>
+                            <li>
+                                <button
+                                    className='sign-out'>Sign out </button>
+                            </li>
+                        </ul>
+                    </div>
+                    <NavLink className='btn btn-primary btn-sm' to="/login">Login</NavLink>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
