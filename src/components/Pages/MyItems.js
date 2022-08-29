b9468726299808fcc4ff69f388f35ec126b94869
Khalidprithy <khalidprithy@gmail.com>
2022-08-30 04:06:40 +0600

    My Item Delete Fixed

--- src/components/Pages/MyItems.js
+++ src/components/Pages/MyItems.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import ShowMyItems from './ShowMyItems';
 
 const MyItems = () => {
     const [user] = useAuthState(auth)
@@ -24,17 +25,18 @@ const MyItems = () => {
         getMyProducts()
     }, [user])
 
-    const handleDeleteBtn = () => {
-        const email = user?.email;
+    const handleDeleteBtn = id => {
         const proceed = window.confirm('Are you sure?');
         if (proceed) {
-            const url = `http://localhost:5000/product?email=${email}`
+            const url = `http://localhost:5000/products/${id}`
             fetch(url, {
                 method: 'DELETE',
             })
                 .then(res => res.json())
                 .then(data => {
-                    console.log(data)
+                    console.log(data);
+                    const remaining = myProducts.filter(product => product._id !== id);
+                    setMyProducts(remaining);
                 })
         }
     }
@@ -42,6 +44,27 @@ const MyItems = () => {
     return (
         <div className='my-4 text-center'>
             <h4 className='text-center font-semibold m-2 text-gray-600'>You have added {myProducts?.length} product</h4>
+            <div>
+                <table className="table m-2">
+                    <thead>
+                        <tr>
+                            <th scope="col">Image</th>
+                            <th className='d-none d-md-block' scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Quantity</th>
+                            <th className='d-none d-md-block' scope="col">Supplier</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    {
+                        myProducts?.map(product => <ShowMyItems
+                            key={product._id}
+                            product={product}
+                            handleDeleteBtn={handleDeleteBtn}
+                        ></ShowMyItems>)
+                    }
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/components/Pages/ShowMyItems.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { AiFillDelete } from 'react-icons/ai';
+
+const ShowMyItems = ({ product, handleDeleteBtn }) => {
+    const { _id } = product;
+
+    return (
+        <tbody>
+            <tr>
+                <td><img className='row-img' src={product?.picture} alt="" /></td>
+                <td className='d-none d-md-block'>
+                    <div className='container'>
+                        <p className='m-0'>{product?.name}</p>
+                        <p className='description'>{product?.description}</p>
+                    </div>
+                </td>
+                <td>${product?.price}</td>
+                <td>
+                    <div className='d-flex align-items-center'>
+                        <p>{product?.quantity}</p>
+                    </div>
+                </td>
+                <td className='d-none d-md-block border-bottom-0'>{product?.supplierName}</td>
+                <td>
+                    <button
+                        onClick={() => handleDeleteBtn(_id)}
+                        className='btn btn-link'><AiFillDelete className='text-danger'></AiFillDelete></button>
+                </td>
+            </tr>
+        </tbody>
+    );
+};
+
+export default ShowMyItems;
